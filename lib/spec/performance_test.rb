ec52e92f7a2d8a345738720b6c295717f6aa88a8
Gavin Morrice <gavin@thinkersplayground.com>
2009-09-13 22:45:04 +0100

    Updated gitignore

---
+++ lib/spec/performance_test.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+require "benchmark"
+
+# runs a quick benchmark to compare the two
+Benchmark.bmbm do |test|
+  test.report("Create 50 new factories") do
+    50.times { Factory :user, :first_name => "John", :admin => false }
+  end
+  
+  until User.count >= 50
+    Factory :user, :last_name => "Smith", :admin => true
+  end
+  
+  test.report("Grab 50 separate factories") do
+    50.times { Grab.one_user :last_name => "Smith", :admin => true }
+  end
+  
+  test.report("Grab 50 factories at once") do
+    Grab.fifty_users :last_name => "Smith", :admin => true
+  end
+  
+end
